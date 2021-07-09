8732b5c89fe46043b03a72ca98fb62a1c835e5b4
wolf30682 <80920822+wolf30682@users.noreply.github.com>
2021-07-09 08:49:08 +0300

    First commit

---
+++ Ints.java
@@ -0,0 +1,7 @@
+package builder_patterns2;
+
+public interface Ints {
+    int sum(int arg0, int arg1);
+    int mult(int arg0, int arg1);
+    int pow(int a, int b);
+}
---
+++ IntsCalculator.java
@@ -0,0 +1,38 @@
+package builder_patterns2;
+
+public class IntsCalculator implements Ints {
+    protected final Calculator target;
+
+    public IntsCalculator() { this.target = new Calculator(); }
+
+    @Override
+    public int sum(int arg0, int arg1) {
+        Calculator calc = new Calculator();
+        return (int) calc.newFormula()
+                .addOperand(arg0)
+                .addOperand(arg1)
+                .calculate(Calculator.Operation.SUM)
+                .result();
+    }
+
+    @Override
+    public int mult(int arg0, int arg1) {
+        Calculator calc = new Calculator();
+        return (int) calc.newFormula()
+                .addOperand(arg0)
+                .addOperand(arg1)
+                .calculate(Calculator.Operation.MULT)
+                .result();
+
+    }
+
+    @Override
+    public int pow(int a, int b) {
+        Calculator calc = new Calculator();
+        return (int) calc.newFormula()
+                .addOperand(a)
+                .addOperand(b)
+                .calculate(Calculator.Operation.POW)
+                .result();
+    }
+}
